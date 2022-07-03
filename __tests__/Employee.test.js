1eb3c74269c52a9d3ba9354063a319973e8fc475
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-07-03 14:38:47 -0500

    employee test

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,25 @@
+const { exportAllDeclaration } = require("@babel/types");
+const { default: test } = require("node:test");
+const Employee = require("../lib/Employee");
+const employee = new Employee("nina", "1110000", "nsmith@gmail.com");
+
+test("test to see if we can get the constructo values for the employee", () => {
+  expect(employee.name).toBe("nina");
+  expect(employee.id).toBe("1110000");
+  expect(employee.email).toBe("nsmith@gmail.com");
+});
+test("test to see if we can get the name from getName()", () => {
+  expect(employee.getName()).toBe("nina");
+});
+
+test("test to see if we can get the id from the getId()", () => {
+  expect(employee.getId()).toBe("1110000");
+});
+
+test("test to see if we can get the email from getEmail()", () => {
+  expect(employee.getEmail()).toBe("nsmith@gmail.com");
+});
+
+test("test if we can get the role from getRole()", () => {
+  expect(employee.getRole()).toBe("Employee");
+});
